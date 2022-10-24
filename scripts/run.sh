ccdddc7c13f4e56a6ec4ab86f8a50bc4e133b73c
albertgoncalves <goncalves.albert@gmail.com>
2022-10-23 21:42:18 -0400

    resolve some `ld` warnings

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,9 +4,15 @@ set -eu
 
 export OCAMLRUNPARAM="b"
 
+flags=(
+    "-fuse-ld=mold"
+    --no-warn-rwx-segments
+    -znoexecstack
+)
+
 "$WD/bin/com" "$1" "$WD/build/main.asm"
 cat "$WD/build/main.asm" >&2
 fasm "$WD/build/main.asm" "$WD/build/main.o" > /dev/null
-ld -fuse-ld=mold -o "$WD/bin/run" -lc "$WD/build/runtime_asm.o" \
+ld "${flags[@]}" -o "$WD/bin/run" -lc "$WD/build/runtime_asm.o" \
     "$WD/build/runtime_c.o" "$WD/build/main.o"
 "$WD/bin/run"
