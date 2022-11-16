d58aa35f2bd8876a2171a122d92035ed8ba10b3f
albertgoncalves <goncalves.albert@gmail.com>
2022-11-15 21:41:40 -0500

    use `clang` instead of `ld`, ditch `musl`

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,15 +4,9 @@ set -eu
 
 export OCAMLRUNPARAM="b"
 
-flags=(
-    "-fuse-ld=mold"
-    --no-warn-rwx-segments
-    -znoexecstack
-)
-
 "$WD/bin/com" "$1" "$WD/build/main.asm"
 cat "$WD/build/main.asm" >&2
 fasm "$WD/build/main.asm" "$WD/build/main.o" > /dev/null
-ld "${flags[@]}" -o "$WD/bin/run" -lc "$WD/build/runtime_asm.o" \
+mold -run clang -no-pie -o "$WD/bin/run" "$WD/build/runtime_asm.o" \
     "$WD/build/runtime_c.o" "$WD/build/main.o"
 "$WD/bin/run"
--- src/runtime.asm
+++ src/runtime.asm
@@ -1,28 +1,37 @@
 format ELF64
 
-public _start
+public main
 
 extrn fflush
-extrn stdout
+extrn setlinebuf
 extrn stderr
+extrn stdout
 
 extrn print_heap
 
 extrn entry_
 
 section '.text' executable
-    _start:
+    main:
+        push rbp
         mov rbp, rsp
+
+        mov rdi, [stderr]
+        call setlinebuf
+
         call entry_
         push rax
+
         call print_heap
+
         mov rdi, [stdout]
         call fflush
         mov rdi, [stderr]
         call fflush
-        pop rdi
-        mov eax, 60
-        syscall
+
+        pop rax
+        leave
+        ret
 
 public HEAP_MEMORY
 
